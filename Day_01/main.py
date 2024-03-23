90fa7d6a9708afc75c0c0d859c19a992d5b3d9be
hkouta <ayanokoji09opp@gmail.com>
2024-03-23 17:35:20 +0000

    Day 1 Completed

---
+++ Day_01/main.py
@@ -0,0 +1,85 @@
+from fastapi import FastAPI , Body
+
+from service import retrieve_posts, retrieve_post_by_id , add_post, update_post_by_id, delete_post_by_id
+
+app = FastAPI()
+
+
+@app.get("/api/v1/posts")
+def get_posts() -> dict:
+
+    posts = retrieve_posts()
+    
+    return {
+        "detail" : "posts retrieved successfully",
+        "status_code" : 200,
+        "data" : posts
+    }
+
+
+@app.get("/api/v1/posts/{id}")
+def get_post(id : int) -> dict : 
+
+    is_post = retrieve_post_by_id(id)
+
+    if is_post is None :
+        return {
+            "detail" : "post doesnt exists",
+            "status_code" : 400,
+            "data" : None
+        }
+    return  {
+        "detail" : "post retrieved successfully",
+        "status_code" : 200,
+        "data" : is_post
+    }
+
+
+@app.post("/api/v1/posts")
+def save_post(payload : dict = Body(...)) -> dict :
+
+    post = add_post(payload)
+
+    return  {
+        "detail" : "post retrieved successfully",
+        "status_code" : 201,
+        "data" : post
+    }
+
+@app.put("/api/v1/posts/{id}")
+def update_post(id : int, payload : dict = Body(...)) -> dict :
+
+    is_exist = update_post_by_id(id, payload)
+
+    if is_exist is None :
+        return {
+            "detail" : "post doesnt exists",
+            "status_code" : 400,
+            "data" : None
+        }
+
+    return  {
+        "detail" : "post updated successfully",
+        "status_code" : 201,
+        "data" : is_exist
+    } 
+
+
+@app.delete("/api/v1/posts/{id}")
+def delete_post(id : int) -> dict :
+
+    is_exist = delete_post_by_id(id)
+
+    if is_exist is None :
+        return {
+            "detail" : "post doesnt exists",
+            "status_code" : 400,
+            "data" : None
+        }
+
+    return  {
+        "detail" : "post deleted successfully",
+        "status_code" : 200,
+        "data" : is_exist
+    } 
+
---
+++ Day_01/service.py
@@ -0,0 +1,58 @@
+from random import randint
+
+from data import posts as _posts
+
+
+def retrieve_posts() -> list :
+    return _posts
+
+def retrieve_post_by_id(id : int) -> dict | None :
+    for p in _posts :
+        if p["id"] == id :
+            return p
+    return None
+
+def add_post(payload) -> None | dict :
+    post = {}
+    id = randint(0, 100)
+    post["id"] = id
+    
+    if "title" in payload :
+        post["title"] = payload["title"]
+
+    if "description" in payload :
+        post["description"] = payload["description"]
+    
+    _posts.append(post)
+    return post
+
+
+def update_post_by_id(id : int, payload) -> dict | None :
+    is_exist = retrieve_post_by_id(id)
+
+    if is_exist is None :
+        return None
+    
+    post = is_exist
+    if "title" in payload :
+        post["title"] = payload["title"]
+
+    if "description" in payload :
+        post["description"] = payload["description"]
+
+    return post
+
+
+def delete_post_by_id(id : int) -> None | dict :
+    is_exist = retrieve_post_by_id(id)
+
+    if is_exist is None :
+        return None
+    
+    _posts.remove(is_exist)
+    return is_exist
+
+
+
+
+
